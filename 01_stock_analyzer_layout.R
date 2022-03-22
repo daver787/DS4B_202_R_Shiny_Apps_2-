cbedf1cfac277670c5c8527bc10d02bb57472a18
David Espinola <daver787@gmail.com>
2022-03-22 09:18:04 -0500

    Added layout script to Shiny app.

---
+++ 01_stock_analyzer_layout.R
@@ -0,0 +1,24 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# STOCK ANALYZER APP - LAYOUT -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Create a basic layout in shiny showing the stock dropdown, interactive plot and commentary
+
+
+# LIBRARIES ----
+library(shiny)
+library(shinyWidgets)
+
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+
+source(file = "00_scripts/stock_analysis_functions.R")
+
+# UI ----
+
+# SERVER ----
+
+# RUN APP ----
