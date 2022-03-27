a0b53bf1c25b2f5aa9f74235fbe6b97c1a576c16
David Espinola <daver787@gmail.com>
2022-03-26 21:08:15 -0500

    Added new script file that will include the server function.

---
+++ 02_stock_analyzer_server.R
@@ -0,0 +1,99 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# STOCK ANALYZER APP - SERVER -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Connect the stock dropdown, interactive plot and commentary using shiny server operations
+
+
+# LIBRARIES ----
+library(shiny)
+library(shinyWidgets)
+
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+
+source(file = "00_scripts/stock_analysis_functions.R")
+
+stock_list_tbl <- get_stock_list("SP500")
+
+stock_data_tbl <- get_stock_data("AAPL", from = "2018-01-01", to = "2019-01-01")
+
+# UI ----
+ui <- fluidPage(
+    title = "Stock Analyzer",
+    
+    # 1.0 HEADER ----
+    div(
+        h1("Stock Analyzer", "by Business Science"),
+        p("This is the first mini-project completed in our", "Expert Shiny Applications Course (DS4B 202-R)")
+    ),
+    
+    # 2.0 APPLICATION UI -----
+    div(
+        column(
+            width = 4, 
+            wellPanel(
+                pickerInput(
+                    inputId = "stock_selection", 
+                    label   = "Stock List (Pick One to Analyze)",
+                    choices = stock_list_tbl$label,
+                    multiple = FALSE, 
+                    selected = stock_list_tbl %>% filter(label %>% str_detect("AAPL")) %>% pull(label),
+                    options = pickerOptions(
+                        actionsBox = FALSE,
+                        liveSearch = TRUE,
+                        size = 10
+                    )
+                ),
+                actionButton(inputId = "analyze", label = "Analyze", icon = icon("download")),
+                verbatimTextOutput(outputId = "selected_symbol")
+            )
+        ),
+        column(
+            width = 8, 
+            div(
+                div(h4(textOutput(outputId = "plot_header" ))),
+                div(
+                    stock_data_tbl %>% plot_stock_data()
+                )
+            )
+        )
+    ),
+    
+    # 3.0 ANALYST COMMENTARY ----
+    div(
+        column(
+            width = 12,
+            div(
+                div(h4("Analyst Commentary")),
+                div(
+                   # stock_data_tbl %>% generate_commentary(user_input = "Placeholder")
+                )
+            )
+        )
+    )
+)
+
+# SERVER ----
+server <- function(input, output, session) {
+    
+    stock_symbol <- eventReactive(input$analyze,{
+        get_symbol_from_user_input(input$stock_selection)
+    }, ignoreNULL = FALSE)
+    
+    output$selected_symbol <- renderText(stock_symbol())
+    
+    plot_header <- eventReactive(input$analyze,{
+        input$stock_selection
+    }, ignoreNULL = FALSE)
+    
+    output$plot_header <- renderText({
+        plot_header()
+    })
+}
+
+# RUN APP ----
+shinyApp(ui = ui, server = server)
